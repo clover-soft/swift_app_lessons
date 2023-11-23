76444730c9b5de245bf246bde69cc897b1156d3b
Yakov <yakov@panargin.ru>
2023-11-23 14:42:23 +0300

    Initial Commit

---
+++ swift_app_lessons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_app_lessons
+//
+//  Created by yakov on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
