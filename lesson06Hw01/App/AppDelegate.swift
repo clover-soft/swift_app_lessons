52b20c73a8d672cd1867ad4af103741f3a1bc568
Yakov <yakov@panargin.ru>
2023-12-24 00:02:56 +0300

    CoreDataModel added

--- lesson06Hw01/App/AppDelegate.swift
+++ lesson06Hw01/App/AppDelegate.swift
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "YourModelName")
+        let container = NSPersistentContainer(name: "CoreDataModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
